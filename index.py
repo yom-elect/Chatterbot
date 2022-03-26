410f4dbc3ac0e7f52e6b2a171661bf3ceab28a81
yomi-playht <oriyomi@play.ht>
2022-03-26 17:58:22 +0000

    intent update

--- index.py
+++ index.py
@@ -1,8 +1,6 @@
 import os
 from flask import Flask, render_template, request, Response
 from flask.helpers import safe_join
-from boto3 import Session
-import base64
 
 
 from intent.model import get_bot_response
@@ -11,13 +9,6 @@ from intent.model import get_bot_response
 app = Flask(__name__)
 static = safe_join(os.path.dirname(__file__), 'static')
 
-session = Session(profile_name="playht-test", region_name="us-east-1")
-polly = session.client("polly")
-
-# polly_client = boto3.Session(
-#                 aws_access_key_id=,                     
-#     aws_secret_access_key=,
-#     region_name='us-west-2').client('polly')
 
 @app.route("/")
 def main():
@@ -27,14 +18,9 @@ def main():
 def get_chatbot_response():
     userText = request.args.get('userMessage')
     botResponse = str(get_bot_response(userText))
-    buffer = polly.synthesize_speech(Text=botResponse, OutputFormat="mp3",
-                                        VoiceId="Joanna")
-    buffer["botResponse"] = botResponse
-    return  Response(buffer["AudioStream"],
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-    
-    
 
+    return botResponse
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
