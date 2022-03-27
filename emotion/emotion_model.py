ca908c18d397b8c968e3d21e548a2d49dc76c67d
yomi-playht <oriyomi@play.ht>
2022-03-27 17:30:14 +0100

    emotional best predictor

--- emotion/emotion_model.py
+++ emotion/emotion_model.py
@@ -15,36 +15,94 @@ from sklearn.model_selection import train_test_split
 # Build Pipeline
 from sklearn.pipeline import Pipeline
 
-# Load Dataset
-df = pd.read_csv("data/emotion_dataset_raw.csv")
 
-# User handles
-df['Clean_Text'] = df['Text'].apply(nfx.remove_userhandles)
+def process_dataset():
+    # Load Dataset
+    df = pd.read_csv("data/emotion_dataset_raw.csv")
 
-# Stopwords
-df['Clean_Text'] = df['Clean_Text'].apply(nfx.remove_stopwords)
+    # User handles
+    df['Clean_Text'] = df['Text'].apply(nfx.remove_userhandles)
 
-# Features & Labels
-Xfeatures = df['Clean_Text']
-ylabels = df['Emotion']
+    # Stopwords
+    df['Clean_Text'] = df['Clean_Text'].apply(nfx.remove_stopwords)
 
-#  Split Data
-x_train,x_test,y_train,y_test = train_test_split(Xfeatures,ylabels,test_size=0.3,random_state=42)
+    # Features & Labels
+    Xfeatures = df['Clean_Text']
+    ylabels = df['Emotion']
 
-# LogisticRegression Pipeline
-pipe_lr = Pipeline(steps=[('cv',CountVectorizer()),('lr',LogisticRegression())])
+    return train_test_split(Xfeatures,ylabels,test_size=0.2,random_state=42)
 
-# Train and Fit Data
-pipe_lr.fit(x_train,y_train)
+def train_emotion_model_logistic_reg():
+    x_train, _ , y_train, _ = process_dataset()
+    # LogisticRegression Pipeline
+    pipe_lr = Pipeline(steps=[('cv',CountVectorizer()),('lr',LogisticRegression())])
 
-# Make A Prediction
-ex1 = "This book was so interesting it made me happy"
+    # Train and Fit Data
+    pipe_lr.fit(x_train,y_train)
 
-pipe_lr.predict([ex1])
+    # Save Model & Pipeline
+    pipeline_file = open("emotion_classifier_model_1.pkl","wb")
+    joblib.dump(pipe_lr,pipeline_file)
+    pipeline_file.close()
 
-# Save Model & Pipeline
-pipeline_file = open("emotion_classifier_model_1.pkl","wb")
-joblib.dump(pipe_lr,pipeline_file)
-pipeline_file.close()
+    return pipe_lr
 
-#pipe_lr =joblib.load("emotion_classifier_model_1.pkl")
+def train_emotion_model_nv_model():
+    x_train, _ , y_train, _ = process_dataset()
+    
+    nv_model = MultinomialNB()
+    # Train and Fit Data
+    nv_model.fit(x_train,y_train)
+
+    # Save Model & Pipeline
+    pipeline_file = open("emotion_nv_classifier_model_1.pkl","wb")
+    joblib.dump(nv_model,pipeline_file)
+    pipeline_file.close()
+
+    return nv_model 
+
+# Fxn
+def predict_nv_emotion(model, docx):
+    cv = CountVectorizer()
+    vect = cv.transform([docx]).toarray()
+    results = model.predict(vect)
+
+    return results[0]
+
+def get_nv_prediction_proba(model, docx):
+    cv = CountVectorizer()
+    vect = cv.transform([docx]).toarray()
+    results = model.predict_proba(vect)
+    return np.max(results)
+
+
+def predict_linear_emotions(model, docx):
+	results = model.predict([docx])
+	return results[0]
+
+def get_linear_prediction_proba(model, docx):
+	results = model.predict_proba([docx])
+	return results
+
+def best_prediction(userResponse):
+    try:
+        pipe_lr = joblib.load("emotion_classifier_model_1.pkl")
+        nv_model = joblib.load("emotion_nv_classifier_model_1.pkl")
+
+        if(get_linear_prediction_proba(pipe_lr, userResponse) >  get_nv_prediction_proba(nv_model, userResponse)):
+            return predict_linear_emotions(pipe_lr, userResponse)
+        else:
+            return predict_nv_emotion(nv_model, userResponse)
+        
+    except:
+        pipe_lr = train_emotion_model_logistic_reg()
+        nv_model = train_emotion_model_nv_model()
+
+        if(get_linear_prediction_proba(pipe_lr, userResponse) >  get_nv_prediction_proba(nv_model, userResponse)):
+            return predict_linear_emotions(pipe_lr, userResponse)
+        else:
+            return predict_nv_emotion(nv_model, userResponse)
+
+
+def predict_emotion(userResponse):
+    return best_prediction(userResponse)
