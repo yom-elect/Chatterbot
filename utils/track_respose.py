7be3a70fb7985b8301bceab621446406da29c17f
yomi-playht <oriyomi@play.ht>
2022-04-21 14:27:36 +0100

    update code error hadling

--- utils/track_respose.py
+++ utils/track_respose.py
@@ -8,22 +8,26 @@ def bot_question(tag, emotion):
     next_index = result_index + 1
 
     if(tag in asides):
-        if (questions[result_index]["all"]):
+        try:
             return questions[result_index]["all"]
-        else:
+        except:
             if (emotion == "neutral"): emotion = "joy"
-            return questions[result_index][emotion]
+            return questions[result_index][emotion]       
 
     if (result_index == 0):
         result.append(emotion)
-        return questions[next_index]["all"]
+        try:
+            return questions[result_index]["all"]
+        except:
+            if (emotion == "neutral"): emotion = "joy"
+            return questions[result_index][emotion]
     elif (result_index < 10):
-        if (questions[next_index]["all"]):
+        try:
             result.append(emotion)
             return questions[next_index]["all"]
-        else:
+        except:
             result.append(emotion)
             if (emotion == "neutral"): emotion = "joy"
-            return questions[next_index][emotion]
+            return questions[next_index][emotion]   
     elif (result_index >= 10):
         return "music time"
